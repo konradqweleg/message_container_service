e75aabe65b25937641c9cfbadbe00f4c116397db
Konrad <konradnrog@gmail.com>
2025-02-27 21:05:22 +0100

    Added tests for insert message

--- src/test/java/com/example/messagecontainer/core/unit/InsertMessageTests.java
+++ src/test/java/com/example/messagecontainer/core/unit/InsertMessageTests.java
@@ -3,6 +3,8 @@ package com.example.messagecontainer.core.unit;
 
 import com.example.messagecontainer.adapter.out.friend_service.FriendServiceAdapter;
 import com.example.messagecontainer.entity.request.*;
+import com.example.messagecontainer.exception.friend_service.UserAreNotFriendsException;
+import com.example.messagecontainer.exception.user_service.UserNotFoundException;
 import com.example.messagecontainer.model.Message;
 import com.example.messagecontainer.port.in.MessagePort;
 import com.example.messagecontainer.port.out.DatabasePort;
@@ -16,8 +18,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 
 
@@ -42,28 +43,109 @@ public class InsertMessageTests {
         Long idUserSender = 1L;
         Long idUserReceiver = 2L;
 
-        when(userServicePort.getUserAboutId(new IdUserDTO(idUserSender))
-                .thenReturn(new UserData(idUserSender, "John", "Doe", "mail@mail.eu")));
+        when(userServicePort.getUserAboutId(new IdUserDTO(idUserSender)))
+                .thenReturn(Mono.just(new UserData(idUserSender, "John", "Doe", "mail@mail.eu")));
 
-        when(userServicePort.getUserAboutId(new IdUserDTO(idUserReceiver))
-                .thenReturn(new UserData(idUserReceiver, "Jane", "Smith", "jane@mail.eu")));
+        when(userServicePort.getUserAboutId(new IdUserDTO(idUserReceiver)))
+                .thenReturn(Mono.just(new UserData(idUserReceiver, "Jane", "Smith", "jane@mail.eu")));
 
-        when(friendServicePort.isFriends(new FriendPairDTO(idUserSender, idUserReceiver)).thenReturn(new IsFriends(true)));
+        when(friendServicePort.isFriends(new FriendPairDTO(idUserSender, idUserReceiver)))
+                .thenReturn(Mono.just(new IsFriends(true)));
 
+        when(databasePort.insertMessage(any()))
+                .thenReturn(Mono.empty());
 
         //when
         Mono<Void> result = messagePort.insertMessage(new MessageDTO("Hello", idUserSender, idUserReceiver));
 
-
         //then
-
         StepVerifier.create(result)
                 .verifyComplete();
 
-        Mockito.verify(databasePort).insertMessage(new Message(anyLong(),"Hello", idUserSender, idUserReceiver, any()));
-
+        Mockito.verify(databasePort, Mockito.times(1)).insertMessage(any(Message.class));
 
     }
 
+    @Test
+    public void whenSenderUserNotFoundShouldReturnUserNotFoundException() {
+        //given
+        Long idUserSender = 1L;
+        Long idUserReceiver = 2L;
+
+        when(userServicePort.getUserAboutId(new IdUserDTO(idUserSender)))
+                .thenReturn(Mono.error(new UserNotFoundException("User not found")));
+
+        when(userServicePort.getUserAboutId(new IdUserDTO(idUserReceiver)))
+                .thenReturn(Mono.just(new UserData(idUserReceiver, "John", "Doe", "mail@mail.eu")));
+
+        when(friendServicePort.isFriends(new FriendPairDTO(idUserSender, idUserReceiver)))
+                .thenReturn(Mono.just(new IsFriends(true)));
+
+        when(databasePort.insertMessage(any()))
+                .thenReturn(Mono.empty());
+
+        //when
+        Mono<Void> result = messagePort.insertMessage(new MessageDTO("Hello", idUserSender, idUserReceiver));
+
+        //then
+        StepVerifier.create(result)
+                .expectError(UserNotFoundException.class).verify();
+
+        Mockito.verify(databasePort, Mockito.times(0)).insertMessage(any(Message.class));
+    }
+
+    @Test
+    public void whenReceiverUserNotFoundShouldReturnUserNotFoundException() {
+        //given
+        Long idUserSender = 1L;
+        Long idUserReceiver = 2L;
+
+        when(userServicePort.getUserAboutId(new IdUserDTO(idUserSender)))
+                .thenReturn(Mono.error(new UserNotFoundException("User not found")));
+
+        when(userServicePort.getUserAboutId(new IdUserDTO(idUserReceiver)))
+                .thenReturn(Mono.just(new UserData(idUserReceiver, "John", "Doe", "mail@mail.eu")));
+
+        when(friendServicePort.isFriends(new FriendPairDTO(idUserSender, idUserReceiver)))
+                .thenReturn(Mono.just(new IsFriends(true)));
+
+        when(databasePort.insertMessage(any()))
+                .thenReturn(Mono.empty());
+
+        //when
+        Mono<Void> result = messagePort.insertMessage(new MessageDTO("Hello", idUserSender, idUserReceiver));
+
+        //then
+        StepVerifier.create(result)
+                .expectError(UserNotFoundException.class).verify();
+
+        Mockito.verify(databasePort, Mockito.times(0)).insertMessage(any(Message.class));
+    }
+
+    @Test
+    public void whenUsersAreNotFriendsShouldReturnError() {
+        //given
+        Long idUserSender = 1L;
+        Long idUserReceiver = 2L;
+
+        when(userServicePort.getUserAboutId(new IdUserDTO(idUserSender)))
+                .thenReturn(Mono.just(new UserData(idUserSender, "John", "Doe", "mail@mail.eu")));
+
+        when(userServicePort.getUserAboutId(new IdUserDTO(idUserReceiver)))
+                .thenReturn(Mono.just(new UserData(idUserReceiver, "Jane", "Smith", "jane@mail.eu")));
+
+        when(friendServicePort.isFriends(new FriendPairDTO(idUserSender, idUserReceiver)))
+                .thenReturn(Mono.just(new IsFriends(false)));
+
+        //when
+        Mono<Void> result = messagePort.insertMessage(new MessageDTO("Hello", idUserSender, idUserReceiver));
+
+        //then
+        StepVerifier.create(result)
+                .expectError(UserAreNotFriendsException.class)
+                .verify();
+
+        Mockito.verify(databasePort, Mockito.never()).insertMessage(any(Message.class));
+    }
 
 }
